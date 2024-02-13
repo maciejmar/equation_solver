d595f60d00d529e205b7e47cec46d6701fa7eae6
maciejmar <memgos@gmail.com>
2024-02-13 23:44:39 +0100

    docker file and ci cd file

--- equation_solver/equation_solver/settings.py
+++ equation_solver/equation_solver/settings.py
@@ -23,13 +23,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$1^y0!n^bm_n0o9is4m80tjf@t*2hr*5^hbdu#0v8vx9+3l+65'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False #turn True by the development
 
 ALLOWED_HOSTS = ALLOWED_HOSTS = [
   'localhost',
   '127.0.0.1',
   'http://localhost:4200',
-  'http://localhost:8001']
+  'http://localhost:8001',
+   'http://localhost:4209',
+  'http://localhost:8001',
+ 'http://vps-9f62e310.vps.ovh.net:8001',
+ 'http://vps-9f62e310.vps.ovh.net:4209' 
+  ]
 
 
 # Application definition
