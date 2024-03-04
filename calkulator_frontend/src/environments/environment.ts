f1c9905fe2de47a9457ae2481b7230ff6be0bd83
maciejmar <memgos@gmail.com>
2024-03-04 08:46:14 +0100

    changing path for local execution

--- calkulator_frontend/src/environments/environment.ts
+++ calkulator_frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8081/calculator/equation_solver/',
+    apiUrl: 'http://localhost:8001/calculator/equation_solver/',
     apiAnotherUrl: 'http://localhost:8001/calculator/another_post/',
     apiOrdinatesUrl: 'http://localhost:8001/calculator/ordinates/'
 
