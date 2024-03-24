b56021ec4fd51212782000d4a6bdbf6f117eeb4e
maciejmar <memgos@gmail.com>
2024-03-24 20:21:25 +0100

    correction of bad endpoint on production

--- calkulator_frontend/src/environments/environment.prod.ts
+++ calkulator_frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://vps-9f62e310.vps.ovh.net/calculator/equation_solver/',
+  apiUrl: 'https://vps-9f62e310.vps.ovh.net/calculator/solve_equations/',
   apiAnotherUrl: 'https://vps-9f62e310.vps.ovh.net/calculator/another_post/',
   apiOrdinatesUrl: 'https://vps-9f62e310.vps.ovh.net/calculator/ordinates/'
 };
