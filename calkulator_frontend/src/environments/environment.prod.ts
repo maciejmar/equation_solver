09899583e7cc021b5873805ba3e35b32096e968c
maciejmar <memgos@gmail.com>
2024-03-31 15:11:02 +0200

    adding 8001 address to endpoints

--- calkulator_frontend/src/environments/environment.prod.ts
+++ calkulator_frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://lequations.mathe.pl/calculator/solve_equations/',
-  apiAnotherUrl: 'https://lequations.mathe.pl/calculator/another_post/',
-  apiOrdinatesUrl: 'https://lequations.mathe.pl/calculator/ordinates/'
+  apiUrl: 'https://lequations.mathe.pl:8001/calculator/solve_equations/',
+  apiAnotherUrl: 'https://lequations.mathe.pl:8001/calculator/another_post/',
+  apiOrdinatesUrl: 'https://lequations.mathe.pl:8001/calculator/ordinates/'
 };
