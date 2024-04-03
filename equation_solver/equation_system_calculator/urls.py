1d1cf63df91b1943c7ae26b2c0c7f3961d1fb676
maciejmar <memgos@gmail.com>
2024-04-03 10:29:48 +0200

    trigger

--- equation_solver/equation_system_calculator/urls.py
+++ equation_solver/equation_system_calculator/urls.py
@@ -19,8 +19,10 @@ from django.urls import path
 from .views.equation_solver_view import EquationSolver, solve, solve_equations
 from .views.another_post_view import AnotherPostView
 from .views.ordinates_post_view import OrdinatesPostView
+from .views.equation_solver_view import csrf_token
 
 urlpatterns = [
+    path('api/csrf_token/', csrf_token, name='csrf_token'),
     path('equation_solver/', EquationSolver.as_view(), name='equation_solver'),
     path('solve/', solve, name='solve'),
     path('solve_equations/', solve_equations, name='solve_equations'),
