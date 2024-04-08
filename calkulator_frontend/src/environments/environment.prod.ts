3e24a64478ec0e87270f336ac3bfceab0752ef91
maciejmar <memgos@gmail.com>
2024-04-08 08:38:17 +0200

    equation_solver endpoint as correct recreated again

--- calkulator_frontend/src/environments/environment.prod.ts
+++ calkulator_frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://lequations.mathe.pl/calculator/solve_equations/',
+  apiUrl: 'https://lequations.mathe.pl/calculator/equation_solver/',
   apiAnotherUrl: 'https://lequations.mathe.pl/calculator/another_post/',
   apiOrdinatesUrl: 'https://lequations.mathe.pl/calculator/ordinates/',
   apiCsrfUrl : 'https://lequations.mathe.pl/calculator/'
